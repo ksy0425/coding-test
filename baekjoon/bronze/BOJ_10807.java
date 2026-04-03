189e5bdf7b69da1b915128e320c1f54677c862c7
ksy0425 <pko04258@gmail.com>
2026-04-03 15:11:36 +0900

    [백준] BOJ_10807 개수 세기

---
+++ baekjoon/bronze/BOJ_10807.java
@@ -0,0 +1,48 @@
+/*
+ * 문제
+ * 총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 정수가 공백으로 구분되어져있다. 셋째 줄에는 찾으려고 하는 정수 v가 주어진다. 입력으로 주어지는 정수와 v는 -100보다 크거나 같으며, 100보다 작거나 같다.
+ *
+ * 출력
+ * 첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_10807 {
+
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private int inputNum;
+    private String[] line;
+    private int target;
+    private int count=0;
+
+    public void input() throws IOException {
+        inputNum = Integer.parseInt(br.readLine());
+    }
+
+    public void inputList() throws IOException {
+        line = br.readLine().split(" ");
+
+        inputTarget();
+
+        for (int i=0; i<inputNum;i++) {
+            if (Integer.parseInt(line[i])==target)
+                count++;
+        }
+        System.out.println(count);
+    }
+
+    public void inputTarget() throws IOException {
+        target = Integer.parseInt(br.readLine());
+    }
+
+    public static void main(String[] args) throws IOException {
+        BOJ_10807 boj_10807 = new BOJ_10807();
+        boj_10807.input();
+        boj_10807.inputList();
+    }
+}
